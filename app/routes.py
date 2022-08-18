45a5262c543e7816a3e07fdc561587d1732c9742
Ibsy Matute <ibsy@rocketmail.com>
2022-08-17 23:14:07 -0500

    added flash to login page which redirects to add_entry.html

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app.forms import PhonebookForm, LoginForm
 from app.models import Entry, LoginUser
 
@@ -11,6 +11,7 @@ def index():
 @app.route('/add_entry', methods=['GET', 'POST'])
 def add_entry():
     form = PhonebookForm()
+
     if form.validate_on_submit():
         print('Form has been validated! Yippee Ki-Yay!')
         name = form.name.data
@@ -19,9 +20,11 @@ def add_entry():
         city = form.city.data
         state = form.state.data
         email = form.email.data
+        
         new_entry = Entry(name=name, phone_number=phone_number, street_address=\
             street_address, city=city, state=state, email=email)
         print(f"{new_entry.name} has been added to entries.")
+        
     return render_template('add_entry.html', form=form)
 
 
@@ -29,11 +32,15 @@ def add_entry():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
-        print('Form has been validate! Yippee Ki-Yay!')
+        print('Form has been validated! Yippee Ki-Yay!')
         email = form.email.data
         password = form.password.data
         logged_in = LoginUser(email=email, password=password)
-        print(f"{logged_in.email} has logged in successfully")
+
+        flash(f"{logged_in.email} has logged in successfully", "success")
+        return redirect(url_for('add_entry'))
+
     return render_template('login.html', form=form)
     
